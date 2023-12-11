8f2cbcf7c80d587911a449b9465b240974b605de
Maks <mhromyk.edu@gmail.com>
2023-12-11 16:46:48 +0100

    initial empty project

---
+++ src/test/java/mh/springboot/restapi/jdbc/JdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package mh.springboot.restapi.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
