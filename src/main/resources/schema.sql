0dc8e9eb314aaf85d603d686d9a861f4536dd947
Maks <mhromyk.edu@gmail.com>
2023-12-12 18:57:29 +0100

    springboot: jdbcTemplate with connection to local MySql as RDBMS

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,3 +1,6 @@
+-- TODO manually: create database should be done manually before running application
+--CREATE DATABASE IF NOT EXISTS spring_quickly;
+
 CREATE TABLE IF NOT EXISTS purchase (
     id INT AUTO_INCREMENT PRIMARY KEY,
     product varchar(100) NOT NULL,
